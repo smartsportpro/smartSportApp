a309192d1b673ab23145a63a5363e1008885e192
Geovanni Parra <gfparra@syr.edu>
2025-10-09 11:57:25 -0400

    Initial Commit

---
+++ smartSport/smartSportApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartSportApp.swift
+//  smartSport
+//
+//  Created by Geovanni Parra on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct smartSportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
