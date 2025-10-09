41278ac8a6725a4a78ad3c8cdd04344a46d444de
Geovanni Parra <parrageovanni04@gmail.com>
2025-10-09 11:57:25 -0400

    Initial Commit

---
+++ smartSport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  smartSport
+//
+//  Created by Geovanni Parra on 10/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
