026ba15a854d1616b75a7360bc92e45a87faf83d
Geovanni Parra <gfparra@syr.edu>
2025-11-03 23:38:46 -0500

    Add files via upload

---
+++ smartSport/Views/Authentication/signUpView.swift
@@ -0,0 +1,18 @@
+//
+//  signUpView.swift
+//  smartSport
+//
+//  Created by Geovanni Parra on 11/4/25.
+//
+
+import SwiftUI
+
+struct signUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    signUpView()
+}
